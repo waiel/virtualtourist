6f76e7ebfacf2b5a558d5874eaad30c41a8e7709
Waiel Eid <waiel@waieleid.com>
2019-05-22 00:13:19 +0300

    Inital Code / view

---
+++ VirtualTourist/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  VirtualTourist
+//
+//  Created by Waiel Eid on 21/5/19.
+//  Copyright © 2019 Waiel Eid. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
